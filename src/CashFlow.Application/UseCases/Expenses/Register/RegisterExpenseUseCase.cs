a9fe591952336fa17ecec50a74f1253ce63e6656
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2024-04-24 22:25:11 -0300

    feat: create endpoint to create expense add validation library to application layer

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,29 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionBase;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        Validate(request);
+
+        return new ResponseRegisterExpenseJson();
+    }
+
+    public void Validate(RequestRegisterExpenseJson request)
+    {
+        var validator = new RegisterExpenseValidator();
+        
+        var validationResult = validator.Validate(request);
+
+        if (!validationResult.IsValid)
+        {
+            var errorMessages = validationResult.Errors.Select(error => error.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errorMessages);
+        }
+    }
+}
---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Communication.Requests;
+using FluentValidation;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+
+public class RegisterExpenseValidator : AbstractValidator<RequestRegisterExpenseJson>
+{
+    public RegisterExpenseValidator()
+    {
+        RuleFor(expense => expense.Title).NotEmpty();
+        RuleFor(expense => expense.Amount).GreaterThan(0);
+        RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow);
+        RuleFor(expense => expense.PaymentType).IsInEnum();
+    }
+}
